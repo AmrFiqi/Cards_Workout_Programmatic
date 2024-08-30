c52d7d5a8106fabf35ebff900e348255a0e45d00
AmrFiqi <amr.h.elfiqi@gmail.com>
2024-08-30 10:38:57 +0300

    Created UIImageView for the card and added it to the viewdidload func

--- Cards Workout/CardSelectionVC.swift
+++ Cards Workout/CardSelectionVC.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class CardSelectionVC: UIViewController {
+    
+    // MARK: - UI Elements
+    
+    let cardImageView = UIImageView()
+    let stopButton = CWButton(backgrounfColor: .systemRed, title: "Stop!")
+    let restartButton = CWButton(backgrounfColor: .systemGreen, title: "Restart")
+    let rulesButton = CWButton(backgrounfColor: .systemBlue, title: "Rules")
 
+    // MARK: - VC Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .red
+        view.backgroundColor = .systemBackground
+        configureUI()
     }
     
+    // MARK: - Functions
+    
+    func configureUI() {
+        configureCardImageView()
+    }
 
+    func configureCardImageView() {
+        view.addSubview(cardImageView)
+        cardImageView.translatesAutoresizingMaskIntoConstraints = false
+        cardImageView.image = UIImage(named: "AS")
+        
+        NSLayoutConstraint.activate([
+            cardImageView.widthAnchor.constraint(equalToConstant: 250),
+            cardImageView.heightAnchor.constraint(equalToConstant: 350),
+            cardImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            cardImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -75)
+        ])
+    }
+    
+    func configureButtons() {
+        
+    }
+    
+    
 }
