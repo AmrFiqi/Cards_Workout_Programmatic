e6e245b98474d15ed72c955697fc9bbdee914776
AmrFiqi <amr.h.elfiqi@gmail.com>
2024-08-30 10:11:40 +0300

    Created the root vc in scene delegate

---
+++ Cards Workout/CardSelectionVC.swift
@@ -0,0 +1,19 @@
+//
+//  CardSelectionVC.swift
+//  Cards Workout
+//
+//  Created by Amr El-Fiqi on 30/08/2024.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+    
+
+}
